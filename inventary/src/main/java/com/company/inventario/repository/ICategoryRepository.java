bc64f7e2ca7f503ddb4fe74817694caf782a5d68
nicolas Godoy <nicodepor3@gmail.com>
2026-05-10 00:04:50 -0300

    Modificacion de IcategoryRepository

--- inventary/src/main/java/com/company/inventario/repository/ICategoryRepository.java
+++ inventary/src/main/java/com/company/inventario/repository/ICategoryRepository.java
@@ -1,9 +1,10 @@
 package com.company.inventario.repository;
 
 import com.company.inventario.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ICategoryRepository extends CrudRepository<Category, Long> {
+public interface ICategoryRepository extends JpaRepository<Category, Long> {
 
 
 
